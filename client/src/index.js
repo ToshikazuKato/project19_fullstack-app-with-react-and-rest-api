d8b317392a7b9902e8889f069fdbe18b1385482b
Toshikazu Horioka <toshikazu@evolany.com>
2019-06-17 10:10:11 +0900

    clear unused package

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { LastLocationProvider } from 'react-router-last-location';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 		<BrowserRouter>
-			<LastLocationProvider>
-				<App />
-		</LastLocationProvider>
+			<App />
 		</BrowserRouter>
 , document.getElementById('root'));
 
